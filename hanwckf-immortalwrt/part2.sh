efeb3fe9786919142a3d4f771346942b3a9838e5
yangxu52 <yyswj10@163.com>
2024-02-19 20:12:35 +0800

    fix🐛: invalid go version '1.21.4': must match format 1.23

--- hanwckf-immortalwrt/part2.sh
+++ hanwckf-immortalwrt/part2.sh
@@ -25,3 +25,8 @@ echo '替换jerrykuku的luci argon主题'
 rm -rf feeds/luci/themes/luci-theme-argon
 git clone -b master --single-branch https://github.com/jerrykuku/luci-theme-argon feeds/luci/themes/luci-theme-argon
 echo '=========Replace luci theme argon OK!========='
+
+echo '替换golang'
+rm -rf feeds/packages/lang/golang
+git clone -b 21.x --single-branch https://github.com/sbwml/packages_lang_golang feeds/packages/lang/golang
+echo '=========Replace golang OK!========='
