c18cc56b3b71479f413f9c77d74232abe58a5d5e
yangxu52 <yyswj10@163.com>
2024-03-07 14:54:53 +0800

    config🔧: default ip addr: 192.168.7.1

--- cmcc-rax3000m/hanwckf-immortalwrt/part3.sh
+++ cmcc-rax3000m/hanwckf-immortalwrt/part3.sh
@@ -20,8 +20,8 @@ cat package/base-files/files/bin/config_generate |grep hostname=
 echo '=========Alert hostname OK!========='
 
 echo '修改默认IP'
-sed -i 's/ipad=${ipaddr:-"192.168.1.1"}/ipad=${ipaddr:-"192.168.7.254"}/g' package/base-files/files/bin/config_generate
+sed -i 's/ipad=${ipaddr:-"192.168.1.1"}/ipad=${ipaddr:-"192.168.7.1"}/g' package/base-files/files/bin/config_generate
 sed -i 's/addr_offset=2/addr_offset=8/g' package/base-files/files/bin/config_generate
-sed -i 's/${ipaddr:-"192.168.$((addr_offset++)).254"}/${ipaddr:-"192.168.$((addr_offset++)).254"}/g' package/base-files/files/bin/config_generate
+sed -i 's/${ipaddr:-"192.168.$((addr_offset++)).1"}/${ipaddr:-"192.168.$((addr_offset++)).1"}/g' package/base-files/files/bin/config_generate
 cat package/base-files/files/bin/config_generate |grep hostname=
 echo '=========Alert Default IP OK!========='
