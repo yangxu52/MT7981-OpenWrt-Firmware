c7fcd52826a17e17920c51d3c287d337e778d8dd
yangxu52 <yyswj10@163.com>
2024-03-07 15:06:48 +0800

    feat✨: add new device: h3c nx30 pro

---
+++ h3c-nx30pro/hanwckf-immortalwrt/part3.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#
+#Copyright 2021-2024 yangxu52<https://github.com/yangxu52>
+#
+#Licensed under the Apache License, Version 2.0 (the "License");
+#you may not use this file except in compliance with the License.
+#You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+#Unless required by applicable law or agreed to in writing, software
+#distributed under the License is distributed on an "AS IS" BASIS,
+#WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#See the License for the specific language governing permissions and
+#limitations under the License.
+
+echo '修改主机名'
+sed -i "s/hostname='ImmortalWrt'/hostname='RAX3000M'/g" package/base-files/files/bin/config_generate
+cat package/base-files/files/bin/config_generate |grep hostname=
+echo '=========Alert hostname OK!========='
+
+echo '修改默认IP'
+sed -i 's/ipad=${ipaddr:-"192.168.1.1"}/ipad=${ipaddr:-"192.168.7.1"}/g' package/base-files/files/bin/config_generate
+sed -i 's/addr_offset=2/addr_offset=8/g' package/base-files/files/bin/config_generate
+sed -i 's/${ipaddr:-"192.168.$((addr_offset++)).1"}/${ipaddr:-"192.168.$((addr_offset++)).1"}/g' package/base-files/files/bin/config_generate
+cat package/base-files/files/bin/config_generate |grep hostname=
+echo '=========Alert Default IP OK!========='
