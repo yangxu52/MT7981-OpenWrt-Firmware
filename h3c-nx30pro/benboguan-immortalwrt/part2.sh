2b31838f38fd0e8c0f22c35150d962bd22dbf3e1
yangxu52 <yyswj10@163.com>
2025-05-17 10:56:34 +0800

    feat✨: bump golang to 1.24.x

--- h3c-nx30pro/benboguan-immortalwrt/part2.sh
+++ h3c-nx30pro/benboguan-immortalwrt/part2.sh
@@ -19,9 +19,9 @@ rm -rf feeds/luci/themes/luci-theme-argon
 git clone -b master --single-branch https://github.com/jerrykuku/luci-theme-argon feeds/luci/themes/luci-theme-argon
 echo '=========Replace luci theme argon OK!========='
 
-echo '替换golang到1.22.x'
+echo '替换golang到1.24.x'
 rm -rf feeds/packages/lang/golang
-git clone -b 22.x --single-branch https://github.com/sbwml/packages_lang_golang feeds/packages/lang/golang
+git clone -b 24.x --single-branch https://github.com/sbwml/packages_lang_golang feeds/packages/lang/golang
 echo '=========Replace golang OK!========='
 
 echo '替换Passwall软件'
