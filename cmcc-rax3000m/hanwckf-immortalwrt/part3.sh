4a58131621ca77f061415088e8f576d6ab0cd82f
yangxu52 <yyswj10@163.com>
2026-02-03 13:08:05 +0800

    feat✨: cmcc rax3000m sync update

--- cmcc-rax3000m/hanwckf-immortalwrt/part3.sh
+++ cmcc-rax3000m/hanwckf-immortalwrt/part3.sh
@@ -29,3 +29,20 @@ sed -i 's/addr_offset=2/addr_offset=8/g' package/base-files/files/bin/config_gen
 sed -i 's/${ipaddr:-"192.168.$((addr_offset++)).1"}/${ipaddr:-"192.168.$((addr_offset++)).1"}/g' package/base-files/files/bin/config_generate
 cat package/base-files/files/bin/config_generate |grep hostname=
 echo '=========Alert Default IP OK!========='
+
+echo '修改NTP服务器'
+sed -i "s/time1.apple.com/ntp.aliyun.com/g" package/base-files/files/bin/config_generate
+sed -i "s/time1.google.com/ntp.tencent.com/g" package/base-files/files/bin/config_generate
+sed -i "s/time.cloudflare.com/time.ustc.edu.cn/g" package/base-files/files/bin/config_generate
+sed -i "s/pool.ntp.org/cn.pool.ntp.org/g" package/base-files/files/bin/config_generate
+echo '=========Alert NTP Server OK!========='
+
+echo '修改闭源驱动2G wifi名称'
+sed -i 's/ssid="ImmortalWrt-2.4G"/ssid="RAX3000M-2.4G"/g' package/mtk/applications/mtwifi-cfg/files/mtwifi.sh
+# sed -i 's/ssid="OpenWRT-2.4G/ssid="RAX3000M-2.4G/g' package/mtk/drivers/wifi-profile/files/common/mt7981/lib/wifi/mtk.sh
+echo '=========Alert 2.4G wifi name OK!========='
+
+echo '修改闭源驱动5G wifi名称'
+sed -i 's/ssid="ImmortalWrt-5G"/ssid="RAX3000M-5G"/g' package/mtk/applications/mtwifi-cfg/files/mtwifi.sh
+# sed -i 's/ssid="OpenWRT-5G/ssid="NX30PRO-5G/g' package/mtk/drivers/wifi-profile/files/common/mt7981/lib/wifi/mtk.sh
+echo '=========Alert 5G wifi name OK!========='
