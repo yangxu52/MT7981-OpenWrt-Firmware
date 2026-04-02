164635f59361473d58c75cc0ef1347a3de4bbaf1
yangxu52 <yyswj10@163.com>
2026-04-03 02:10:53 +0800

    test: forge package

--- profiles/common/scripts/pre-feeds.sh
+++ profiles/common/scripts/pre-feeds.sh
@@ -9,4 +9,8 @@ prepend_feed_if_missing() {
 echo '添加Passwall依赖feeds'
 prepend_feed_if_missing 'src-git passwall_luci https://github.com/Openwrt-Passwall/openwrt-passwall.git;main'
 prepend_feed_if_missing 'src-git passwall_packages https://github.com/Openwrt-Passwall/openwrt-passwall-packages.git;main'
-echo '=========Add passwall feeds source OK!========='
+echo '=========Add passwall feeds OK!========='
+
+echo '添加openwrt-forge feed'
+prepend_feed_if_missing 'src-git forge https://github.com/yangxu52/openwrt-forge;openwrt-21.02-rust-1.94.1'
+echo '=========Add openwrt-forge feed OK!========='
